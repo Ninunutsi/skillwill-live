e8856c8c1858c0b1239acf1638b035b9026d55a5
Ninunutsi <khurtsidzenino4@gmail.com>
2023-10-04 18:57:51 +0400

    lecture 1

---
+++ script.js
@@ -0,0 +1,16 @@
+// rogor mushaobs vebi
+// failis struqtura
+// monacemta tipebi
+// cvladebi
+// == ===
+// conditionals
+// funqcia abrunebs tu ara rames anonimuri function keyword arrow
+
+
+for(let i =0 ; i< 10; i++){
+    let row = ''
+    for(let n = 0; n < 10; n ++){
+        row += ' * '
+    }
+    console.log(row)
+}
